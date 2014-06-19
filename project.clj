2ce1ed95ddf703b6f4bd499fdf7a9a78ba0e6254
Roman Scherer <roman.scherer@burningswell.com>
2014-06-19 11:57:02 +0200

    Version 0.1.3-SNAPSHOT

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject listora/env.test "0.1.2"
+(defproject listora/env.test "0.1.3-SNAPSHOT"
   :description "REPL functions for testing environ based projects."
   :url "https://github.com/listora/env.test"
   :license {:name "Eclipse Public License"
